9859e49873c160fb3405b4dcef69d6b52e98c4c2
Romel Pérez <ronelprhone@gmail.com>
2015-08-21 18:06:25 -0500

    feat: ejemplo de estructura de app en javascript

---
+++ javascript/structure/js/app.js
@@ -0,0 +1,21 @@
+// Aplicación JavaScript Básica.
+
+(function () {
+
+  // Objeto aplicación.
+  var app = {
+
+    // Asignar eventos al DOM.
+    init: function () {
+      // ...
+    },
+
+    main: function () {
+      // ...
+    }
+
+  };
+
+  document.addEventListener('load', app.init);
+
+})();
