42c581ee0bddec0bf01a1bc450dc6590248a80d6
Romel Pérez <ronelprhone@gmail.com>
2015-08-21 19:19:15 -0500

    update: funciones en JavaScript y diseño en la página de JavaScript básico

--- javascript/basic/js/functions.js
+++ javascript/basic/js/functions.js
@@ -27,6 +27,54 @@ var objeto1 = {
 
 
 //
+// Visibilidad de variables.
+// Ejemplo tomado de Douglas Crockford, Javascript The Good Parts, página 36.
+//
+
+// Contexto de variables por función.
+var fn1 = function () {
+  var a = 3;
+  var b = 5;
+
+  var fnInterna = function () {
+    var b = 7;
+    var c = 11;
+    // En este punto, a vale 3, b vale 7, y c vale 11
+
+    a += b + c;
+    // En este punto, a vale 21, b vale 7, y c vale 11
+  };
+  // En este punto, a vale 3, b vale 5, y c no está definida
+
+  fnInterna();
+  // En este punto, a vale 21, b vale 5
+};
+
+// No hay contextos por bloques, sólo por funciones.
+var fn2 = function () {
+  var a = 10;
+
+  if (true) {
+    var b = 20;
+    // En este punto, a y b son visibles.
+  }
+  // En este punto, a y b son visibles.
+
+  for (var i=0; i<3; i++) {
+    var c = 30;
+    // En es punto, a, b y c son visibles.
+  }
+  // En es punto, a, b y c son visibles.
+
+  var fn = function () {
+    var d = 40;
+    // En es punto, a, b, c y d son visibles.
+  };
+  // En es punto, a, b y c son visibles, pero d no.
+};
+
+
+//
 // Polimorfismo.
 //
 
@@ -73,3 +121,45 @@ var o2 = {prop: false, numero: 3};
 
 console.log(multiplicar(n1, n2));  // 24
 console.log(multiplicar(o1, o2));  // {numero: 12}
+
+
+//
+// Paso de variables.
+//
+
+// Las variables de tipo primitivo se pasan por copia, mientras que las variables
+// tipo objeto se pasan por referencia.
+
+// Pasado por valor.
+var a = 'un string';
+var b = a;
+b = 'string cambiado';
+
+if (a === b) console.log('iguales');
+else         console.log('desiguales');  // 'desiguales'
+
+// Pasado por referencia.
+var c = {};
+var d = c;
+d.cambiado = true;
+
+if (c === d) console.log('iguales');  // 'iguales'
+else         console.log('desiguales');
+
+// Pasado por valor en una función.
+var fn1 = function (primitivo) {
+  primitivo = 50;
+};
+var e = 34;
+fn1(e);
+console.log(e);  // 34
+
+// Pasado por referencia en una función.
+var fn2 = function (objeto) {
+  objeto.cambiado = true;
+};
+var objeto = {
+  cambiado: false
+};
+fn2(objeto);
+console.log(objeto.cambiado);  // true
